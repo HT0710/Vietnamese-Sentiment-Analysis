7068fe7a6f8f5bb65c1e7351c5cba3bae67ddbcf
ArtA <pthung7102002@gmail.com>
2023-05-14 04:33:22 +0700

    update train and utils

---
+++ train.py
@@ -0,0 +1,85 @@
+from utils import train_step, test_step, save_model, build_vocabulary, encode_words, padding
+from torch.utils.data import DataLoader, random_split
+from models import RNN, LSTM, GRU
+from data import DataModule
+import torch.optim as optim
+import torch.nn as nn
+import pandas as pd
+import torch
+import os
+
+
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+NUM_WOKER = os.cpu_count()
+
+# Impact performance
+TRAIN_TEST_SIZE = (0.8, 0.2)
+SEQ_LENGTH = 256
+EMBEDDING_SIZE = 256
+HIDDEN_SIZE = 512
+NUM_LAYER = 2
+DROPOUT = 0.25
+BATCH_SIZE = 333
+LEARNING_RATE = 0.001
+EPOCHS = 10
+
+# Load dataset
+dataset = pd.read_csv('datasets/IMDB_processed.csv')
+data, label = dataset['text'].tolist(), dataset['label'].tolist()
+
+# Build vocabulary
+vocab = build_vocabulary(data)
+
+# Encode words
+data_encoded = encode_words(data, vocab)
+
+# padding sequences
+features = padding(data=data_encoded, pad_id=vocab['<PAD>'], seq_length=SEQ_LENGTH)
+
+# Create data module
+dataset = DataModule(features, label)
+
+# Train, test split
+train_data, test_data = random_split(dataset, TRAIN_TEST_SIZE)
+
+# Create dataloader
+train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WOKER)
+test_loader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WOKER)
+
+# Model
+model = LSTM(
+    vocab_size=len(vocab),
+    output_size=1,
+    hidden_size=HIDDEN_SIZE,
+    embedding_size=EMBEDDING_SIZE,
+    n_layers=NUM_LAYER,
+    dropout=DROPOUT
+).to(DEVICE)
+
+# Loss, optimizer and learning rate scheduler
+criterion = nn.BCELoss()
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.1, patience=2)
+
+for epoch in range(EPOCHS):
+    print(f"Epoch: {epoch+1} | Lr: {optimizer.param_groups[0]['lr']}")
+
+    train_loss, train_acc = train_step(
+        model=model, dataloader=train_loader, criterion=criterion, optimizer=optimizer, device=DEVICE
+    )
+
+    test_loss, test_acc = test_step(
+        model=model, dataloader=test_loader, criterion=criterion, device=DEVICE
+    )
+
+    scheduler.step(test_loss)
+
+    print(f"  |- Loss: {train_loss:.4f}  Acc: {train_acc:.4f} | Test_loss: {test_loss:.4f}  Test_acc: {test_acc:.4f}")
+
+    if (epoch+1) % 1 == 0:
+        save_model(model=model, target_dir="checkpoints", model_name=f"E{epoch+1}_L{test_loss:.4f}_A{test_acc:.2f}.pth")
--- utils.py
+++ utils.py
@@ -1,80 +1,123 @@
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from nltk.stem import PorterStemmer, WordNetLemmatizer
-from nltk.corpus import stopwords
-import nltk
-import re, yaml
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchmetrics.functional import accuracy
+from collections import Counter
+from pathlib import Path
+from tqdm import tqdm
+import numpy as np
 
-stopwords_list = stopwords.words('english')
 
-def setup():
-    # Prepare
-    nltk.download('punkt')
-    nltk.download('wordnet')
-    nltk.download('stopwords')
 
-# Remove html
-def remove_html(text):
-    pattern = r'<.*?>'
-    out = re.sub(pattern, ' ', text)
-    return out
+def build_vocabulary(data: list):
+    words = ' '.join(data).split(' ')                       # Join all the text data and split it into individual words
+    counter = Counter(words)                                # Count the occurrences of each word
+    vocab = sorted(counter, key=counter.get, reverse=True)  # Sort the words in descending order of frequency
+    int2word = dict(enumerate(vocab, 1))                    # Create a mapping from index to word (int to word)
+    int2word[0] = '<PAD>'                                   # Add a special token for padding at index 0
+    word2int = {word: id for id, word in int2word.items()}  # Create a mapping from word to index (word to int)
+    return word2int
 
-# Remove special characters
-def remove_specials(text, remove_digits=True):
-    pattern = r'[^a-zA-z0-9\s]'
-    out = re.sub(pattern, ' ', text)
-    return out
 
-# Stemming
-def stemming(text):
-    stemmer = PorterStemmer()
-    tokens = nltk.word_tokenize(text)
-    stems = [stemmer.stem(word) for word in tokens]
-    return ' '.join(stems)
+def encode_words(data: list, vocab: dict):
+    encoded = []
+    for seq in tqdm(data, 'Encode'):
+        seq_idx = []
+        for word in seq.split():
+            seq_idx.append(vocab[word])
+        encoded.append(seq_idx)
+    return encoded
 
-# lemmatization
-def lemmatization(text):
-    lemmatizer = WordNetLemmatizer()
-    tokens = nltk.word_tokenize(text)
-    lemmas = [lemmatizer.lemmatize(word) for word in tokens]
 
-    return ' '.join(lemmas)
+def padding(data, pad_id, seq_length=128):
+    features = np.full((len(data), seq_length), pad_id, dtype=int)
+    for i, row in enumerate(data):
+        features[i, :len(row)] = np.array(row)[:seq_length]
+    return features
 
-# Remove stopwords
-def remove_stopwords(text):
-    tokens = nltk.word_tokenize(text)
-    removed = [word for word in tokens if word not in stopwords_list]
-    return ' '.join(removed)
 
-# Bag of words
-def bow(dataset, train=True, ngrams=(1, 1)):
-    vectorizer = CountVectorizer(min_df=0, max_df=1, ngram_range=ngrams)
-    if train:
-        bow = vectorizer.fit_transform(dataset)
-    else:
-        bow = vectorizer.transform(dataset)
-    return bow.toarray()
+def train_step(
+        model: nn.Module,
+        dataloader: DataLoader,
+        criterion: nn.Module,
+        optimizer: optim.Optimizer,
+        device: torch.device = 'cpu'
+):
+    """Train function"""
 
-# IF-IDF
-def ifidf(dataset, train=True, ngrams=(1, 1)):
-    vectorizer = TfidfVectorizer(min_df=2, max_df=0.5, ngram_range=ngrams)
-    if train:
-        bow = vectorizer.fit_transform(dataset)
-    else:
-        bow = vectorizer.transform(dataset)
-    return bow.toarray()
+    # Train mode
+    model.train()
 
-# Save result
-def save_result(prepocess: list, vectorizer: str, model: str, accuracy: float):
-    with open('result.yaml', 'r') as file:
-        result = yaml.safe_load(file)
-        last_result = list(result.keys())[-1]
-        num = int(last_result.split('_')[-1])
-        result[f'run_{num+1}'] = {
-            "prepocess": prepocess,
-            "vectorizer": vectorizer,
-            "model": model,
-            "accuracy": accuracy
-        }
-        
-    with open('result.yaml', 'w') as file:
-        yaml.dump(result, file)
+    # Define
+    train_loss, train_acc = 0, 0
+    data_size = len(dataloader)
+
+    # Train loop
+    for step, (X, y) in enumerate(dataloader, 1):
+        X, y = X.to(device), y.to(device)
+
+        outputs = model(X).squeeze()
+        loss = criterion(outputs, y.float())
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        train_acc += accuracy(outputs, y, task='binary')
+
+        print(f'  |- Train step: {step}/{data_size} | Loss: {train_loss/step:.4f}  Acc: {train_acc/step:.4f}     ', end='\r')
+
+    train_loss = train_loss / data_size
+    train_acc = train_acc / data_size
+
+    return train_loss, train_acc
+
+
+def test_step(
+        model: nn.Module,
+        dataloader: DataLoader,
+        criterion: nn.Module,
+        device: torch.device = 'cpu'
+):
+    """Test function"""
+
+    # Evaluate mode
+    model.eval()
+
+    # Define
+    test_loss, test_acc = 0, 0
+    data_size = len(dataloader)
+
+    # Evaluate loop
+    with torch.inference_mode():
+        for step, (X, y) in enumerate(dataloader, 1):
+            X, y = X.to(device), y.to(device)
+
+            outputs = model(X).squeeze()
+            loss = criterion(outputs, y.float())
+
+            test_loss += loss.item()
+            test_acc += accuracy(outputs, y, task='binary')
+
+            print(f'  |- Test step: {step}/{data_size} | Loss: {test_loss/step:.4f}  Acc: {test_acc/step:.4f}     ', end='\r')
+
+    test_loss = test_loss / data_size
+    test_acc = test_acc / data_size
+
+    return test_loss, test_acc
+
+
+def save_model(model: nn.Module, target_dir: str, model_name: str):
+    """Save model"""
+    
+    target_dir_path = Path(target_dir)
+    target_dir_path.mkdir(parents=True, exist_ok=True)
+
+    # Create model save path
+    assert model_name.endswith(".pth") or model_name.endswith(".pt"), "model_name should end with '.pt' or '.pth'"
+    model_save_path = target_dir_path / model_name
+
+    # Save the model state_dict()
+    torch.save(obj=model.state_dict(), f=model_save_path)
