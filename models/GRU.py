f91b2250a526ab7a2dc7289a3441e220e85aecdb
ArtA <pthung7102002@gmail.com>
2023-05-22 03:08:32 +0700

    inherit GRU to litmodel

--- models/GRU.py
+++ models/GRU.py
@@ -1,10 +1,22 @@
+from lightning_modules.model import LitModel
+import torch.nn.functional as F
+import torch.optim as optim
 import torch.nn as nn
 
 
-
-class GRU(nn.Module):
-    def __init__(self, vocab_size, output_size, hidden_size=128, embedding_size=400, n_layers=2, dropout=0.2):
+class GRU(LitModel):
+    def __init__(
+            self,
+            vocab_size: int,
+            output_size: int,
+            hidden_size: int = 128,
+            embedding_size: int = 400,
+            n_layers: int = 2,
+            dropout: float = 0.2,
+            save_hparams = None
+        ):
         super().__init__()
+        self.save_hyperparameters('save_hparams')
         self.embedding = nn.Embedding(vocab_size, embedding_size)
         self.gru = nn.GRU(embedding_size, hidden_size, n_layers, dropout=dropout, batch_first=True)
         self.dropout = nn.Dropout(0.3)
@@ -20,3 +32,10 @@ class GRU(nn.Module):
         out = self.fc(out)
         out = self.sigmoid(out)
         return out
+
+    def criterion(self, y_hat, y):
+        return F.binary_cross_entropy(y_hat, y)
+
+    def configure_optimizers(self):
+        learning_rate = self.hparams.save_hparams['trainer']['learning_rate']
+        return optim.Adam(self.parameters(), lr=learning_rate)
