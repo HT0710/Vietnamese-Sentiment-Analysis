3989b7fe4c974cb42e51c3ec6a749ed357626cf9
ArtA <pthung7102002@gmail.com>
2023-05-31 16:15:33 +0700

    add callback config

--- lightning_modules/callback.py
+++ lightning_modules/callback.py
@@ -3,7 +3,7 @@ import lightning.pytorch.callbacks as cb
 from rich import print
 
 
-class TrainerCallback(cb.Callback):
+class PrintTrainResult(cb.Callback):
     def on_train_epoch_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
         epoch = trainer.current_epoch
         results = trainer.callback_metrics
@@ -20,11 +20,29 @@ class TrainerCallback(cb.Callback):
         print(f"[bold]Epoch[/]( {epoch} )  [bold]Train[/]({result_train})  [bold]Val[/]({result_val})")
 
 
-CALLBACKS = [
-    cb.RichModelSummary(),
-    cb.RichProgressBar(),
-    cb.LearningRateMonitor(logging_interval='epoch'),
-    cb.ModelCheckpoint(monitor='val/loss', save_weights_only=True),
-    cb.EarlyStopping(monitor='val/loss', min_delta=0.001, patience=2),
-    TrainerCallback(),
-]
+def callbacks_list(config):
+    cb_list = [
+        PrintTrainResult(),
+        cb.RichModelSummary(),
+        cb.RichProgressBar(),
+        cb.LearningRateMonitor(logging_interval='epoch'),
+    ]
+
+    cb_list.append(
+        cb.ModelCheckpoint(
+            monitor='val/loss',
+            dirpath=config['checkpoint']['dirpath'],
+            filename=config['checkpoint']['filename'],
+            save_weights_only=config['checkpoint']['weights_only']
+        )
+    ) if config['checkpoint']['enable'] else None
+
+    cb_list.append(
+        cb.EarlyStopping(
+            monitor='val/loss',
+            min_delta=0.001,
+            patience=config['earlystopping']['patience']
+        )
+    ) if config['earlystopping']['enable'] else None
+
+    return cb_list
