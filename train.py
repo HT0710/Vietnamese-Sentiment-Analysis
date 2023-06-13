0c5a40208e1bdb2cfa296039ded24fd62a90ea75
ArtA <pthung7102002@gmail.com>
2023-06-13 23:37:06 +0700

    update cli, ...

--- train.py
+++ train.py
@@ -21,17 +21,23 @@ def main(config):
 
     # Dataset
     config['data']['num_workers'] = os.cpu_count() if torch.cuda.is_available() else 0
-    dataset = data.IMDBDataModule(preprocessing=preprocesser, **config['data'])
+    dataset = data.VietDataModule(preprocessing=preprocesser, **config['data'])
 
     # Model
     config['model']['vocab_size'] = dataset.vocab_size
-    model = models.RNNParallel(**config['model'])
+    model = models.GRU(**config['model'])
     model.save_hparams(config)
+
+    # Continue
+    if config['trainer']['weight']:
+        model.load(config['trainer']['weight'])
 
     # Trainer
     trainer = Trainer(
+        logger=True,
         max_epochs=config['trainer']['num_epochs'],
-        callbacks=callbacks_list(config['callback'])
+        callbacks=callbacks_list(config['callback']),
+        enable_checkpointing=config['callback']['checkpoint']['enable'],
     )
 
     trainer.fit(model, dataset)
@@ -44,16 +50,19 @@ if __name__=="__main__":
     parser.add_argument("-e", "--epoch", type=int, default=None)
     parser.add_argument("-b", "--batch", type=int, default=None)
     parser.add_argument("-lr", "--learning_rate", type=float, default=None)
+    parser.add_argument("-w", "--weight", type=str, default=None)
     args = parser.parse_args()
 
     with open('config.yaml', 'r') as file:
         config = yaml.full_load(file)
 
-    if args.epoch is not None:
+    if args.epoch:
         config['trainer']['num_epochs'] = args.epoch
-    if args.batch is not None:
+    if args.batch:
         config['data']['batch_size'] = args.batch
-    if args.learning_rate is not None:
+    if args.learning_rate:
         config['trainer']['learning_rate'] = args.learning_rate
+    if args.weight:
+        config['trainer']['weight'] = args.weight
 
     main(config)
